d97f69a8429d25554359db99cb4bdf1e2e05b57c
Jahid Uddin <jahidudd65@gmail.com>
2024-12-21 10:55:29 +0100

    [update]: create ui

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,24 +1,151 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
 	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"time"
 
 	"github.com/jahidxuddin/uni-drop/internal/filetransfer"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	go startGRPCServer()
+
+	waitForServer(":9000")
+
+	go func() {
+		err := openURL("http://localhost:50000")
+		if err != nil {
+			log.Println("Failed to open default web browser:", err)
+		}
+	}()
+
+	http.Handle("/script.js", http.FileServer(http.Dir("static")))
+	http.Handle("/style.css", http.FileServer(http.Dir("static")))
+	http.Handle("/index.html", http.FileServer(http.Dir("static")))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			handleFileUpload(w, r)
+		} else {
+			http.ServeFile(w, r, "static/index.html")
+		}
+	})
+
+	log.Println("Starting web server on port 50000")
+	if err := http.ListenAndServe(":50000", nil); err != nil {
+		log.Fatalf("Failed to start HTTP server: %v", err)
+	}
+}
+
+func startGRPCServer() {
 	lis, err := net.Listen("tcp", ":9000")
 	if err != nil {
 		log.Fatalf("Failed to listen on port 9000: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-
 	filetransfer.RegisterFileTransferServer(grpcServer, &filetransfer.Server{})
 
+	log.Println("Starting grpc server on port 9000")
 	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve gRPC server over port 9000: %v", err)
+		log.Fatalf("Failed to serve gRPC server: %v", err)
 	}
+}
+
+func waitForServer(address string) {
+	for {
+		conn, err := net.Dial("tcp", address)
+		if err == nil {
+			conn.Close()
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+}
+
+func handleFileUpload(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseMultipartForm(10 << 20)
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	files := r.MultipartForm.File["file"]
+	if len(files) == 0 {
+		http.Error(w, "No files uploaded", http.StatusBadRequest)
+		return
+	}
+
+	uploadDir := "uploads"
+	if err := createUploadDirectory(uploadDir); err != nil {
+		http.Error(w, "Unable to create upload directory", http.StatusInternalServerError)
+		return
+	}
+
+	for _, fileHeader := range files {
+		err := saveUploadedFile(fileHeader, uploadDir)
+		if err != nil {
+			http.Error(w, "Unable to save file", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func createUploadDirectory(uploadDir string) error {
+	if _, err := os.Stat(uploadDir); os.IsNotExist(err) {
+		return os.Mkdir(uploadDir, os.ModePerm)
+	}
+	return nil
+}
+
+func saveUploadedFile(fileHeader *multipart.FileHeader, uploadDir string) error {
+	file, err := fileHeader.Open()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	filePath := filepath.Join(uploadDir, fileHeader.Filename)
+
+	outFile, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = io.Copy(outFile, file)
+	return err
+}
+
+func openURL(url string) error {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "rundll32"
+		args = []string{"url.dll,FileProtocolHandler", url}
+	case "darwin":
+		cmd = "open"
+		args = []string{url}
+	case "linux":
+		cmd = "xdg-open"
+		args = []string{url}
+	default:
+		return fmt.Errorf("unsupported platform")
+	}
+
+	return exec.Command(cmd, args...).Start()
 }
---
+++ static/script.js
@@ -0,0 +1,137 @@
+const dropArea = document.getElementById("drop-area");
+const fileInput = document.getElementById("file-input");
+const fileListSection = document.getElementById("file-list-section");
+const fileList = document.getElementById("uploaded-files-list");
+const sendFilesBtn = document.getElementById("send-files-btn");
+
+let allFiles = [];
+
+fileInput.addEventListener("change", (e) => {
+	if (fileInput instanceof HTMLInputElement) {
+		addFiles(fileInput.files);
+	}
+});
+
+["dragenter", "dragover"].forEach((eventName) => {
+	dropArea.addEventListener(eventName, (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		dropArea.classList.add("border-blue-500", "bg-blue-100");
+	});
+});
+
+["dragleave", "drop"].forEach((eventName) => {
+	dropArea.addEventListener(eventName, (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		dropArea.classList.remove("border-blue-500", "bg-blue-100");
+	});
+});
+
+dropArea.addEventListener("drop", (e) => {
+	e.preventDefault();
+	if (fileInput instanceof HTMLInputElement) {
+		addFiles(e.dataTransfer.files);
+	}
+});
+
+function addFiles(newFiles) {
+	const currentFiles = Array.from(newFiles);
+
+	const dataTransfer = new DataTransfer();
+
+	allFiles.forEach((file) => dataTransfer.items.add(file));
+
+	currentFiles.forEach((file) => {
+		if (!allFiles.some((f) => f.name === file.name)) {
+			dataTransfer.items.add(file);
+			allFiles.push(file);
+		}
+	});
+
+	fileInput.files = dataTransfer.files;
+
+	updateFileList(allFiles);
+	adjustDropAreaWidth();
+}
+
+function updateFileList(files) {
+	fileList.innerHTML = "";
+
+	files.forEach((file) => {
+		const listItem = document.createElement("li");
+		listItem.classList.add(
+			"file-item",
+			"flex",
+			"justify-between",
+			"bg-gray-100",
+			"p-2",
+			"my-2",
+			"rounded-md",
+			"shadow-sm",
+			"transition-colors",
+			"hover:bg-blue-100"
+		);
+		listItem.setAttribute("data-filename", file.name);
+
+		const fileInfo = document.createElement("div");
+		fileInfo.classList.add("flex", "space-x-2", "items-center");
+
+		const fileName = document.createElement("span");
+		fileName.textContent = file.name;
+		fileInfo.appendChild(fileName);
+
+		const fileSize = document.createElement("span");
+		fileSize.textContent = `(${(file.size / 1024).toFixed(2)} KB)`;
+		fileSize.classList.add("text-sm", "text-gray-500");
+		fileInfo.appendChild(fileSize);
+
+		listItem.appendChild(fileInfo);
+
+		const removeButton = document.createElement("button");
+		removeButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" class="text-white"><path d="M12 10.586l4.95-4.95 1.414 1.414L13.414 12l4.95 4.95-1.414 1.414L12 13.414l-4.95 4.95-1.414-1.414L10.586 12l-4.95-4.95 1.414-1.414L12 10.586z"/></svg>`;
+		removeButton.classList.add(
+			"text-xs",
+			"p-1",
+			"ml-4",
+			"focus:outline-none"
+		);
+
+		removeButton.addEventListener("click", () => {
+			allFiles = allFiles.filter((f) => f.name !== file.name);
+
+			updateFileList(allFiles);
+			fileInput.files = new DataTransfer().files;
+
+			if (allFiles.length === 0) {
+				sendFilesBtn.classList.add("hidden");
+			}
+			adjustDropAreaWidth();
+		});
+
+		listItem.appendChild(removeButton);
+		fileList.appendChild(listItem);
+	});
+
+	if (files.length > 0) {
+		sendFilesBtn.classList.remove("hidden");
+		fileListSection.classList.remove("hidden");
+	} else {
+		sendFilesBtn.classList.add("hidden");
+		fileListSection.classList.add("hidden");
+	}
+}
+
+function adjustDropAreaWidth() {
+	if (allFiles.length === 0) {
+		dropArea.classList.add("w-full");
+		dropArea.classList.remove("md:w-1/2");
+	} else {
+		dropArea.classList.add("md:w-1/2");
+		dropArea.classList.remove("w-full");
+	}
+}
+
+function handleSendFiles() {
+	document.getElementById("file-form").submit();
+}
